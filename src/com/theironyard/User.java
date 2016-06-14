84dea2f6ca2705265cd5f8718206a7747579c72d
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-06-14 11:47:02 -0400

    Add Database Functionality

--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -2,10 +2,17 @@ package com.theironyard;
 
 
 public class User {
+    int id;
     String name;
     String password;
 
     public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(int id, String name, String password) {
+        this.id = id;
         this.name = name;
         this.password = password;
     }
